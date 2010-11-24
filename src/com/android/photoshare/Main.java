e579b990918f01913ca63741adfa617d06698796
Ikai Lan <ikai.lan@gmail.com>
2010-11-23 18:14:38 -0800

    Adding ability to select an image

--- src/com/android/photoshare/Main.java
+++ src/com/android/photoshare/Main.java
@@ -3,28 +3,46 @@ package com.android.photoshare;
 import java.util.Date;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class Main extends Activity {
-	
+    private static final int SELECT_IMAGE = 0;
+
     Button uploadButton;
     TextView text;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        uploadButton = (Button) findViewById(R.id.upload_photo);
-        text = (TextView) findViewById(R.id.text_view);
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.main);
+	uploadButton = (Button) findViewById(R.id.upload_photo);
+	text = (TextView) findViewById(R.id.text_view);
     }
-    
+
     public void selectPhoto(View view) {
 	Date date = new Date();
 	text.setText(date.toString());
+	// Guess this can also be EXTERNAL_CONTENT_URI
+	startActivityForResult(new Intent(Intent.ACTION_PICK,
+		android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI),
+		SELECT_IMAGE);
+
     }
-    
-    
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	super.onActivityResult(requestCode, resultCode, data);
+	if (requestCode == SELECT_IMAGE)
+	    if (resultCode == Activity.RESULT_OK) {
+		Uri selectedImage = data.getData();
+		text.setText(selectedImage.toString());
+	    }
+    }
+
 }
